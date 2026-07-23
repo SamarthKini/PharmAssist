26ef24559672e7d607fe7c612b37631bf24bb094
SamarthKini <samarthkini@outlook.com>
2026-07-24 01:32:31 +0530

    New Database update

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,21 +2,40 @@ import os
 from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
+# import boto3
+# import psycopg2
 
 load_dotenv()
-DB_USER = os.getenv("RDS_USERNAME", "postgres")
-DB_PASSWORD = quote_plus(os.getenv("RDS_PASSWORD", "your_password"))
-DB_ENDPOINT = os.getenv("RDS_ENDPOINT", "database-1-instance-1.cazyekw6gpnq.us-east-1.rds.amazonaws.com")
-DB_PORT = os.getenv("RDS_PORT", "5432")
-DB_NAME = os.getenv("RDS_DB_NAME", "postgres")
+DB_ENDPOINT = os.getenv("RDS_ENDPOINT")
+DB_PORT = int(os.getenv("RDS_PORT", 5432))
+DB_NAME = os.getenv("RDS_DB_NAME")
+# DB_USER = os.getenv("RDS_IAM_USER")
+# AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+DB_USERNAME=os.getenv("RDS_USERNAME")
+DB_PASSWORD = os.getenv("RDS_PASSWORD")
 
-postgres_url = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_ENDPOINT}:{DB_PORT}/{DB_NAME}"
-# print(postgres_url)
-engine = create_engine(
-    postgres_url, 
-    connect_args={"sslmode": "require"}, 
-    echo=True
-)
+# connection_
+
+# def get_iam_token():
+#     rds = boto3.client("rds", region_name=AWS_REGION)
+#     return rds.generate_db_auth_token(
+#         DBHostname=DB_ENDPOINT,
+#         Port=DB_PORT,
+#         DBUsername=DB_USER
+#     )
+
+# def connect_with_iam():
+#     token = get_iam_token()
+#     return psycopg2.connect(
+#         host=DB_ENDPOINT,
+#         port=DB_PORT,
+#         user=DB_USER,
+#         password=token,
+#         dbname=DB_NAME,
+#         sslmode="require"
+#     )
+
+engine = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_ENDPOINT}:{DB_PORT}/{DB_NAME}", echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
